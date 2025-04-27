58b5ad196050e52a2f3cb6214b4ca7c8a6f3aded
yassirab02 <yassirabourial86@gmail.com>
2025-04-27 18:37:58 +0100

    adding a lot of changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import HomePage from './pages/HomePage';
-import StillsPage from './pages/StillsPage';
-import MotionPage from './pages/MotionPage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import PortfolioPage from './pages/PortfolioPage';
-import NotFoundPage from './pages/NotFoundPage';
-import { AnimatePresence } from 'framer-motion';
-import { SpeedInsights } from "@vercel/speed-insights/react"
-
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import StillsPage from "./pages/StillsPage";
+import MotionPage from "./pages/MotionPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import PortfolioPage from "./pages/PortfolioPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { AnimatePresence } from "framer-motion";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <>
-    <AnimatePresence mode="wait">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="stills" element={<StillsPage />} />
-          <Route path="motion" element={<MotionPage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="contact" element={<ContactPage />} />
-          <Route path="portfolio" element={<PortfolioPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </AnimatePresence>
-    <SpeedInsights />
+      <SpeedInsights />
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="stills" element={<StillsPage />} />
+            <Route path="motion" element={<MotionPage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="contact" element={<ContactPage />} />
+            <Route path="portfolio" element={<PortfolioPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
