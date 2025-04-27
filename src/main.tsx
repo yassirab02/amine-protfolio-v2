a87b4ecab6a2d143f11e6425a0aeb4c9af914ed3
yassirab02 <yassirabourial86@gmail.com>
2025-04-27 18:25:05 +0100

    adding a lot of changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import App from './App.tsx';
 import './index.css';
 
